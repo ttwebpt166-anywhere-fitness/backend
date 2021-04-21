2b1348dc37b664b4a53b4c02c8bc537d72807d60
Jesse Medrano <Jtamedrano@gmail.com>
2021-04-21 16:22:28 -0700

    cookie sessions added

--- src/middleware/validateJwt.js
+++ src/middleware/validateJwt.js
@@ -3,21 +3,11 @@ const { url } = require("../../constants");
 const { findUser } = require("../routes/auth/models");
 
 module.exports = async (req, res, next) => {
-  const { authorization } = req.headers;
-  const token = authorization.split(" ")[1];
-  if (!token) {
-    res.status(404).json({
-      error: {
-        message: "Not Authorized",
-        redirectLink: `${url}/auth/login`,
-      },
-    });
-    return;
-  }
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    if (decoded.exp > Date.now()) {
-      res.status(400).json({
+  if (!req.user?.username) {
+    const { authorization } = req.headers;
+    const token = authorization.split(" ")[1];
+    if (!token) {
+      res.status(404).json({
         error: {
           message: "Not Authorized",
           redirectLink: `${url}/auth/login`,
@@ -25,26 +15,39 @@ module.exports = async (req, res, next) => {
       });
       return;
     }
-    const user = await findUser(decoded.userId);
-    if (!user.username) {
-      res.status(400).json({
-        error: {
-          message: "Not Authorized",
-          redirectLink: `${url}/auth/login`,
-        },
-      });
-      return;
-    }
-    req.user = user;
-  } catch (error) {
-    if (error.message.includes("jwt expired")) {
-      res.status(400).json({
-        error: {
-          message: "Not Authorized",
-          redirectLink: `${url}/auth/login`,
-        },
-      });
-      return;
+    try {
+      const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+      if (decoded.exp > Date.now()) {
+        res.status(400).json({
+          error: {
+            message: "Not Authorized",
+            redirectLink: `${url}/auth/login`,
+          },
+        });
+        return;
+      }
+      const user = await findUser(decoded.userId);
+      if (!user.username) {
+        res.status(400).json({
+          error: {
+            message: "Not Authorized",
+            redirectLink: `${url}/auth/login`,
+          },
+        });
+        return;
+      }
+      req.session.userId = user.id;
+      req.user = user;
+    } catch (error) {
+      if (error.message.includes("jwt expired")) {
+        res.status(400).json({
+          error: {
+            message: "Not Authorized",
+            redirectLink: `${url}/auth/login`,
+          },
+        });
+        return;
+      }
     }
   }
   next();
