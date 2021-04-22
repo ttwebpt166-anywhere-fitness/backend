9bd7bb014d3b46f8fe94a9e735aa5f471e1a6920
Anil_Ramcharran <anil.gitme@gmail.com>
2021-04-21 23:34:35 -0400

    added a classes table in migrations

--- src/data/migrations/20210421043427_one.js
+++ src/data/migrations/20210421043427_one.js
@@ -1,20 +1,33 @@
-exports.up = function (knex) {
-  return knex.schema.createTable("users", (users) => {
-    users.increments("id").primary();
-    users.string("username").unique();
-    users.string("password");
-    users.string("city");
-    users.string("state");
-    users.string("zip");
-    users.enum("account_type", ["student", "teacher"], {
-      useNative: true,
-      enumName: "account_type",
+exports.up = function(knex) {
+    knex.schema.createTable("users", (users) => {
+        users.increments("id").primary();
+        users.string("username").unique();
+        users.string("password");
+        users.string("city");
+        users.string("state");
+        users.string("zip");
+        users.enum("account_type", ["student", "teacher"], {
+            useNative: true,
+            enumName: "account_type",
+        });
+        users.timestamp("created_on").defaultTo(knex.fn.now());
+        users.timestamp("updated_on").defaultTo(knex.fn.now());
     });
-    users.timestamp("created_on").defaultTo(knex.fn.now());
-    users.timestamp("updated_on").defaultTo(knex.fn.now());
-  });
+    knex.schema.createTable('classes', table => {
+        table.increments();
+        table.string('name').NotNullable();
+        table.string('type').NotNullable();
+        table.string('start_time').NotNullable();
+        table.string('date').NotNullable()
+        table.string('duration').NotNullable();
+        table.string('intensity_level').NotNullable();
+        table.string('location').NotNullable();
+        table.integer('number_of_attendees').NotNullable();
+        table.integer('max_attendees').NotNullable();
+    })
 };
 
-exports.down = function (knex) {
-  return knex.schema.dropTableIfExist("users");
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExist("users")
+        .dropTableIfExist('classes');
 };
