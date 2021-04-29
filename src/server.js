6bdeb84a82f9ea88e4accea3b9671fec42ad1ad0
Anil_Ramcharran <anil.gitme@gmail.com>
2021-04-29 00:35:51 -0400

    added model for classes

--- src/server.js
+++ src/server.js
@@ -11,11 +11,12 @@ const connectRedis = require("connect-redis");
 const Redis = require("redis");
 const { __prod__ } = require("../constants");
 const checkSession = require("./middleware/checkSession");
+const classHandler = require("./routes/classes");
 
 const app = Express();
 
 if (process.env.NODE_ENV !== "production") {
-  app.use(logger);
+    app.use(logger);
 }
 
 const RedisStore = connectRedis(session);
@@ -24,22 +25,23 @@ const redis = Redis.createClient(process.env.REDIS_URL);
 app.use(cors());
 app.use(express.json());
 app.use(
-  session({
-    name: "qid",
-    cookie: {
-      maxAge: 1000 * 30 * 60 * 60 * 24,
-      httpOnly: true,
-      sameSite: "lax",
-      secure: __prod__,
-    },
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: new RedisStore({ client: redis, disableTouch: true }),
-  })
+    session({
+        name: "qid",
+        cookie: {
+            maxAge: 1000 * 30 * 60 * 60 * 24,
+            httpOnly: true,
+            sameSite: "lax",
+            secure: __prod__,
+        },
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: new RedisStore({ client: redis, disableTouch: true }),
+    })
 );
 
 app.use("/v1/auth", auth);
 app.use("/v1/", checkSession, validateJwt, root);
+app.use("/v1/class", checkSession, validateJwt, classHandler);
 
 module.exports = app;
