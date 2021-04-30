ba333b4ce11941ec4199d826fef97d2247642f0a
Jtamedrano <Jtamedrano@gmail.com>
2021-04-29 21:10:35 -0700

    fixed code

--- src/data/dbConfig.js
+++ src/data/dbConfig.js
@@ -1,4 +1,6 @@
-const environment = process.env.NODE_ENV.replace(" ", "") || "development";
+const environment = process.env.NODE_ENV
+  ? process.env.NODE_ENV.replace(" ", "")
+  : "development";
 const knex = require("knex");
 const knexConfig = require("../../knexfile");
 module.exports = knex(knexConfig[environment]);
